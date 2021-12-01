333acb721292bd770f1adbc86202142386428aaf
Yigit Serin <yigitserin@gmail.com>
2021-12-01 08:52:47 +0300

    First day solutions.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var result = -1
+        var lastTotal = 0
+
+        input.forEach { currentValue ->
+            if (currentValue > lastTotal){
+                result += 1
+            }
+            lastTotal = currentValue
+        }
+
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var result = -1
+        var lastTotal = 0
+
+        input.forEachIndexed { index, currentValue ->
+            if(input.size > index + 2){
+                val currentTotal = currentValue + input[index+1] + input[index+2]
+                if (currentTotal > lastTotal){
+                    result += 1
+                }
+                lastTotal = currentTotal
+            }
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputInt("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputInt("Day01")
     println(part1(input))
     println(part2(input))
-}
+ }
